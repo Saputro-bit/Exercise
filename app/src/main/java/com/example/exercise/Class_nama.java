ef515334e57110a825abd5d602230914d01e220b
Saputro-bit <muhammadnoorsaputro23@gmail.com>
2021-04-07 03:43:14 +0700

    first commit

---
+++ app/src/main/java/com/example/exercise/Class_nama.java
@@ -0,0 +1,17 @@
+package com.example.exercise;
+
+public class Class_nama {
+    private String Nama;
+
+    //inisialisasi objek
+    public Class_nama(String nama)
+    {
+        this.Nama = nama;
+    }
+
+    //Membuuat method getName untuk mengembalikan nilai le variabel Nama
+    public String getNama()
+    {
+        return this.Nama;
+    }
+}
